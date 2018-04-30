0650f779d16c2798d40fb11db0be447956029b3a
Zbynek Roubalik <zroubali@redhat.com>
2018-04-30 10:38:07 +0200

    add more WebWaiters

--- src/main/java/cz/xtf/radanalytics/waiters/WebWaiters.java
+++ src/main/java/cz/xtf/radanalytics/waiters/WebWaiters.java
@@ -1,5 +1,8 @@
 package cz.xtf.radanalytics.waiters;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,5 +12,17 @@ public class WebWaiters {
 
 	public static void waitUntilElementIsVisible(WebElement webElement, WebDriver webDriver) {
 		new WebDriverWait(webDriver, 30).until(ExpectedConditions.visibilityOf(webElement));
+	}
+
+	public static void waitUntilElementIsPresent(String xpath, WebDriver webDriver, int timeOutInSeconds) {
+		new WebDriverWait(webDriver, timeOutInSeconds).until(presenceOfElementLocated(By.xpath(xpath)));
+	}
+
+	public static void waitUntilElementIsPresent(String xpath, WebDriver webDriver) {
+		waitUntilElementIsPresent(xpath, webDriver, 30);
+	}
+
+	public static void waitUntilElementIsNotPresent(String xpath, WebDriver webDriver) {
+		new WebDriverWait(webDriver, 30).until(ExpectedConditions.numberOfElementsToBe(By.xpath(xpath), 0));
 	}
 }
