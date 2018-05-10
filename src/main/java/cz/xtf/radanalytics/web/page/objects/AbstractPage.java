43daacd012bf50d5b34ef75fbfb36a639498d511
ohonchar <ksisaev@gmail.com>
2018-05-10 15:35:57 +0300

    added refactored Jupiter and Sparknotebook; added extended field decorators v1.0, fixed checkstyle, added page loader in AbstractPage.class

--- src/main/java/cz/xtf/radanalytics/web/page/objects/AbstractPage.java
+++ src/main/java/cz/xtf/radanalytics/web/page/objects/AbstractPage.java
@@ -2,6 +2,7 @@ package cz.xtf.radanalytics.web.page.objects;
 
 import cz.xtf.radanalytics.waiters.WebWaiters;
 import cz.xtf.radanalytics.web.extended.elements.ExtendedFieldDecorator;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -12,31 +13,25 @@ import java.net.URL;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 public abstract class AbstractPage {
-
 	public final String hostname;
 	public final WebDriver webDriver;
 
 	public AbstractPage(WebDriver webDriver, String hostname, boolean navigateToPage, String navigateToPageUrl) {
 		this.webDriver = webDriver;
 		this.hostname = hostname;
-
 		PageFactory.initElements(new ExtendedFieldDecorator(webDriver), this);
 		WebWaiters.setDriver(webDriver);
-
 		if (navigateToPage) {
-
 			if (!Objects.equals(webDriver.getCurrentUrl(), navigateToPageUrl)) {
-				pageLoaded(2000L,webDriver, navigateToPageUrl, 3);
+				pageLoaded(2000L, navigateToPageUrl, 3);
 				webDriver.get(navigateToPageUrl);
 			}
 		}
 	}
 
-	private static void pageLoaded(Long interval, WebDriver webDriver, String url, int countTimes){
+	private static void pageLoaded(Long interval, String url, int countTimes) {
 		URL link = null;
 		try {
 			link = new URL(url);
@@ -44,11 +39,9 @@ public abstract class AbstractPage {
 			log.error(String.format("The following url provided is malformed: %s", url));
 			log.error(e.getMessage());
 		}
-
 		URL finalLink = link;
 		BooleanSupplier successConditionForConnection = () -> {
 			try {
-
 				HttpURLConnection connection = (HttpURLConnection) finalLink.openConnection();
 				connection.setRequestMethod("GET");
 				connection.connect();
@@ -58,7 +51,6 @@ public abstract class AbstractPage {
 				return false;
 			}
 		};
-
 		int countTries = 0;
 		while (!successConditionForConnection.getAsBoolean()) {
 			countTries++;
