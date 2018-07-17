340fd1ae1a8392fe27cf2c8f3733a89c278a6bc7
ohonchar <ksisaev@gmail.com>
2018-07-17 16:50:54 +0300

    refactored ProjectPage in jupyter notebook

--- src/main/java/cz/xtf/radanalytics/notebook/sparknotebook/page/objects/ApplicationPage.java
+++ src/main/java/cz/xtf/radanalytics/notebook/sparknotebook/page/objects/ApplicationPage.java
@@ -71,7 +71,7 @@ public class ApplicationPage extends AbstractPage {
 		};
 
 		try {
-			WebWaiters.waitFor(successCondition, null, 10000L, 9000000L);
+			WebWaiters.waitFor(successCondition, null, 10 * 1000L,  150*60*1000L);
 		} catch (InterruptedException | TimeoutException e) {
 			log.error(e.getMessage());
 		}
